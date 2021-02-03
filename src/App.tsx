a68fa931a36605ef81c8f104ef95175b41a17d77
kg782 <kg0839@gmail.com>
2021-02-03 18:36:19 +1100

    Connected Graph QL

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PHOTOS = gql`
+  query Photos($search: String, $page: Int, $limit: Int) {
+    photos(
+      options: {
+        paginate: { page: $page, limit: $limit }
+        search: { q: $search }
+      }
+    ) {
+      data {
+        id
+        title
+        url
+        thumbnailUrl
+      }
+      meta {
+        totalCount
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_PHOTOS, {
+    variables: {
+      search: 'est',
+      page: 1,
+      limit: 5,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
