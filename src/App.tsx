1ef1aa22d4f28b17407ce546c2e52915a2292a75
kg782 <kg0839@gmail.com>
2021-02-03 19:37:51 +1100

    Search query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import { useQuery, gql } from '@apollo/client';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+interface Photo {
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+interface PhotosData {
+  photos: {
+    data: Photo[];
+  };
+}
+
+interface PhotosVars {
+  q: string;
+  page: number;
+  limit: number;
+}
 
 const GET_PHOTOS = gql`
-  query Photos($search: String, $page: Int, $limit: Int) {
+  query Photos($q: String, $page: Int, $limit: Int) {
     photos(
-      options: {
-        paginate: { page: $page, limit: $limit }
-        search: { q: $search }
-      }
+      options: { paginate: { page: $page, limit: $limit }, search: { q: $q } }
     ) {
       data {
         id
@@ -25,36 +46,58 @@ const GET_PHOTOS = gql`
 `;
 
 function App() {
-  const { loading, error, data } = useQuery(GET_PHOTOS, {
+  const [q, setQ] = useState<string>('');
+  const { error, data } = useQuery<PhotosData, PhotosVars>(GET_PHOTOS, {
     variables: {
-      search: 'est',
+      q,
       page: 1,
       limit: 5,
     },
   });
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => setQ(event.target.value),
+    [setQ]
+  );
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
   console.log(data);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Form as={Row}>
+        <Form.Group controlId="search" as={Col}>
+          <Form.Control
+            type="text"
+            placeholder="Search keywords on title"
+            value={q}
+            onChange={handleChange}
+          />
+        </Form.Group>
+      </Form>
+      <Table striped bordered hover as={Row}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Thumbnail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.photos &&
+            data.photos.data.map((photo) => (
+              <tr key={photo.id}>
+                <td>{photo.id}</td>
+                <td>{photo.title}</td>
+                <td>
+                  <Image src={photo.thumbnailUrl} rounded />
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const client = new ApolloClient({
   uri: 'https://graphqlzero.almansi.me/api',
