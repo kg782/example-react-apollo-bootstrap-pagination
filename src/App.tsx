4e72262ba782ed8c2b9597d49c66962e4a66e215
kg782 <kg0839@gmail.com>
2021-02-03 20:41:32 +1100

    Modal image

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { useQuery, gql } from '@apollo/client';
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
-import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
+import ImageRow from './components/ImageRow';
 
-interface Photo {
+export interface Photo {
   id: number;
   title: string;
   url: string;
@@ -18,6 +21,9 @@ interface Photo {
 interface PhotosData {
   photos: {
     data: Photo[];
+    meta: {
+      totalCount: number;
+    };
   };
 }
 
@@ -47,10 +53,12 @@ const GET_PHOTOS = gql`
 
 function App() {
   const [q, setQ] = useState<string>('');
+  const [page, setPage] = useState<number>(1);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
   const { error, data } = useQuery<PhotosData, PhotosVars>(GET_PHOTOS, {
     variables: {
       q,
-      page: 1,
+      page,
       limit: 5,
     },
   });
@@ -58,46 +66,81 @@ function App() {
     (event: React.ChangeEvent<HTMLInputElement>) => setQ(event.target.value),
     [setQ]
   );
+  const handleSelectPhoto = useCallback(
+    (photo: Photo) => setSelectedPhoto(photo),
+    [setSelectedPhoto]
+  );
+  const handleModalHide = useCallback(() => setSelectedPhoto(null), [
+    setSelectedPhoto,
+  ]);
 
   if (error) return <p>Error</p>;
 
   console.log(data);
 
   return (
-    <Container className="mt-5">
-      <Form as={Row}>
-        <Form.Group controlId="search" as={Col}>
-          <Form.Control
-            type="text"
-            placeholder="Search keywords on title"
-            value={q}
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form>
-      <Table striped bordered hover as={Row}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Thumbnail</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.photos &&
-            data.photos.data.map((photo) => (
-              <tr key={photo.id}>
-                <td>{photo.id}</td>
-                <td>{photo.title}</td>
-                <td>
-                  <Image src={photo.thumbnailUrl} rounded />
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </Container>
+    <div>
+      <Container className="mt-5">
+        <Form as={Row}>
+          <Form.Group controlId="search" as={Col}>
+            <Form.Control
+              type="text"
+              placeholder="Search keywords on title"
+              value={q}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+        <Table striped bordered hover as={Row}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Thumbnail</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.photos &&
+              data.photos.data.map((photo) => (
+                <ImageRow
+                  key={photo.id}
+                  photo={photo}
+                  onSelect={handleSelectPhoto}
+                />
+              ))}
+          </tbody>
+        </Table>
+        <Row>
+          <Col>
+            <Pagination>
+              <Pagination.First />
+              <Pagination.Prev />
+              <Pagination.Item>{1}</Pagination.Item>
+              <Pagination.Ellipsis />
+
+              <Pagination.Item>{10}</Pagination.Item>
+              <Pagination.Item>{11}</Pagination.Item>
+              <Pagination.Item active>{12}</Pagination.Item>
+              <Pagination.Item>{13}</Pagination.Item>
+              <Pagination.Item disabled>{14}</Pagination.Item>
+
+              <Pagination.Ellipsis />
+              <Pagination.Item>{20}</Pagination.Item>
+              <Pagination.Next />
+              <Pagination.Last />
+            </Pagination>
+          </Col>
+        </Row>
+      </Container>
+      <Modal show={selectedPhoto} onHide={handleModalHide}>
+        <Modal.Header closeButton />
+
+        <Modal.Body>
+          <Image src={selectedPhoto?.url} className="w-100" />
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 }
 
---
+++ src/components/ImageRow.tsx
@@ -0,0 +1,24 @@
+import React, { useCallback } from 'react';
+import Image from 'react-bootstrap/Image';
+import { Photo } from '../App';
+
+type ImageRowProps = {
+  photo: Photo;
+  onSelect: (photo: Photo) => void;
+};
+
+function ImageRow({ photo, onSelect }: ImageRowProps) {
+  const handleClick = useCallback(() => onSelect(photo), [onSelect, photo]);
+
+  return (
+    <tr onClick={handleClick}>
+      <td>{photo.id}</td>
+      <td>{photo.title}</td>
+      <td>
+        <Image src={photo.thumbnailUrl} rounded />
+      </td>
+    </tr>
+  );
+}
+
+export default ImageRow;
